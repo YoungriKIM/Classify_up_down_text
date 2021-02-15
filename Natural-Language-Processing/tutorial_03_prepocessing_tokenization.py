ce196044f5a4ebc18fcaaef1ff0945faf85e500d
YoungriKIM <lemontleo0311@gmail.com>
2021-02-15 23:47:00 +0900

    2021-02-15

--- Natural-Language-Processing/tutorial_03_prepocessing_tokenization.py
+++ Natural-Language-Processing/tutorial_03_prepocessing_tokenization.py
@@ -2,13 +2,30 @@
 # 토큰의 단위가 상황에 따라 다르지만, 보통 의미있는 단위로 토큰을 정의합니다.
 
 
-#1. 단어 토큰화: 토큰의 기준이 단어일 경우
-
+#1. 단어 토큰화: 토큰의 기준이 단어일 경우 (각 기능에 대한 것은 사용할 떄 찾아볼 것)
+# 1) word_tokenize
 from nltk.tokenize import word_tokenize
 print(word_tokenize("Don't be a fooled by the dark sounding name, Ms. Hanna's Orphanage is as cheery as cheery goes for a party shop."))
 
 # ['Do', "n't", 'be', 'a', 'fooled', 'by', 'the', 'dark', 'sounding', 'name', ',', \
-# 'Ms.', 'Hanna', "'s", 'Orphanage', 'is', 'as', 'cheery', 'as', 'cheery', 'goes', 'for', 'a', 'party', 'shop', '.']
+# 'Ms.', 'Hanna', "'s", 'Orphanage', 'is', 'as', 'cheery', 'as', 'cheery', 'goes', \
+# 'for', 'a', 'party', 'shop', '.']
 
+# 2) WordPunctTokenizer
+from nltk.tokenize import WordPunctTokenizer
+print(WordPunctTokenizer().tokenize("Don't be a fooled by the dark sounding name, Ms. Hanna's Orphanage is as cheery as cheery goes for a party shop."))
+# ['Don', "'", 't', 'be', 'a', 'fooled', 'by', 'the', 'dark', 'sounding', 'name', ','\
+# , 'Ms', '.', 'Hanna', "'", 's', 'Orphanage', 'is', 'as', 'cheery', 'as', 'cheery', \
+# 'goes', 'for', 'a', 'party', 'shop', '.']
 
-from nltk.tokenize import WordPunctTokenizer  
+# 3) text_to_word_sequence
+from tensorflow.keras.preprocessing.text import text_to_word_sequence
+print(text_to_word_sequence("Don't be a fooled by the dark sounding name, Ms. Hanna's Orphanage is as cheery as cheery goes for a party shop."))
+# ["don't", 'be', 'a', 'fooled', 'by', 'the', 'dark', 'sounding', 'name', 'ms', \
+# "hanna's", 'orphanage', 'is', 'as', 'cheery', 'as', 'cheery', 'goes', 'for', \
+# 'a', 'party', 'shop']
+
+# 4) TreebankWordTokenizer
+from nltk.tokenize import TreebankWordTokenizer
+tokenizer = TreebankWordTokenizer()
+text = "Starting a home-based restaurant may be an ideal. it doesn't have a food chain or restaurant of their own."
