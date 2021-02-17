79c9a06ee4e70f48613523bd94a39ed2d7fdd079
YoungriKIM <lemontleo0311@gmail.com>
2021-02-17 20:50:01 +0900

    2021-02-17

--- Natural-Language-Processing/z_practice_RNN_Naver_movie_reviw.py
+++ Natural-Language-Processing/z_practice_RNN_Naver_movie_reviw.py
@@ -126,3 +126,41 @@ for sentence in train_data['document']:
     temp_x = [word for word in temp_x if not word in stopwords]
     x_train.append(temp_x)
 print(x_train[:3])
+# 잘 된 것 확인
+
+# test >> 토큰화 + 불용어 제거 적용
+x_test = []
+for sentence in test_data['document']:
+    temp_x = []
+    temp_x = okt.morphs(sentence, stem=True)
+    temp_x = [word for word in temp_x if not word in stopwords]
+    x_test.append(temp_x)
+print(x_test[:3])
+# 확인
+
+# 정수 인코딩(텍스트를 숫자로)
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(x_train)
+print('단어에 정수 부여 확인\n', tokenizer.word_index)
+
+
+# 등장 빈도수 3회 미만 단어의 비중 확인
+threshold = 3
+total_cnt = len(tokenizer.word_index)
+rare_cnt = 0
+total_freq = 0
+rare_freq = 0
+
+# 단어-빈도수의 쌍을 key와 value로 받는다.
+for key, value in tokenizer.word_counts.items():
+    total_freq = total_freq + value
+
+    # 단어 등장 빈도수가 threshold(3) 미만이면
+    if(value < threshold):
+        rare_cnt = rare_cnt + 1
+        rare_freq = rare_freq + value
+
+print('단어 집합(vocabulary)의 크기 :',total_cnt)
+print('등장 빈도가 %s번 이하인 희귀 단어의 수: %s'%(threshold - 1, rare_cnt))
+print("단어 집합에서 희귀 단어의 비율:", (rare_cnt / total_cnt)*100)
+print("전체 등장 빈도에서 희귀 단어 등장 빈도 비율:", (rare_freq / total_freq)*100)
