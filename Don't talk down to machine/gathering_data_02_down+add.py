8dac70a7541005298b7d61276a84894beaf8c57d
YoungriKIM <lemontleo0311@gmail.com>
2021-02-19 19:43:57 +0900

    2021-02-19

--- Don't talk down to machine/gathering_data_02_down+add.py
+++ Don't talk down to machine/gathering_data_02_down+add.py
@@ -1,2 +1,62 @@
 # 데이터셋 부족하다고 판단, 더 모아서 csv로 저장 함
+# 저장 완료
+# csv_down_data = pd.read_csv('../NLP/save/down_data_02.csv', index_col=0)
+
+
+import numpy as np
+import pandas as pd
+import re
+
+
+# 전처리에 쓸 pre_data 정의
+def pre_data(data):
+    temp = data.copy()
+    temp = temp.drop(['번호','뒷문맥','출전'],1)    # 필요없는 거 떨구고
+    temp = temp.fillna('.')     # nan 값 . 으로
+    temp['검색어'] = temp['앞문맥'].str.cat(temp['검색어'].astype(str))     # 한 문장으로 합병
+    temp['앞문맥'] = 0      # 라벨링 0으로(반말)
+    return temp
+
+# down 데이터 한번에 모으기
+df_test = []
+for i in range(1, 65):
+    file_path = "../NLP/sample_data/down_4_sum/down_data ("+str(i)+").xls"
+    temp = pd.read_excel(file_path)
+    temp = pre_data(temp)
+    df_test.append(temp)
+down_data = pd.concat(df_test)
+
+# 열 이름 변경
+down_data.columns = ['label', 'data']
+
+# 중복값 몇개인지 확인
+print(len(down_data['label'].unique()))     # 1 > 라벨이 0으로 하나이니 ok
+print('data열의 중복값: ', int(len(down_data['data']) - int(len(down_data['data'].unique())))) 
+# data열의 중복값: 232
+
+# 중복값 제거
+down_data.drop_duplicates(subset = ['data'], inplace = True)
+print('중복값 제거 후의 데이터 수: ', len(down_data))
+# 중복값 제거 후의 데이터 수:  2870
+
+# 한글 아닌 문자 제외
+down_data['data'] = down_data['data'].str.replace("[^ㄱ-ㅎㅏ-ㅣ가-힣 ]","")
+print('down_data.shape: ', down_data.shape)    # (2870, 2)
+print('down_data.tail:\n ', down_data.tail())
+
+
+# ------------------------------------------------------------
+# ------------------------------------------------------------
+
+# 다음 파일에서 up + down 하기 위해 저장해보자
+print(type(down_data))  # <class 'pandas.core.frame.DataFrame'>
+
+# ------ csv -------
+# 저장하기
+down_data.to_csv('../NLP/save/down_data_02.csv')
+# 읽어와서 확인
+csv_down_data = pd.read_csv('../NLP/save/down_data_02.csv', index_col=0)
+print('load한 반말 데이터: \n', csv_down_data[-5:])
+
+### 용량: 110KB
 
--- Don't talk down to machine/p_08_add_data.py
+++ Don't talk down to machine/p_08_add_data.py
@@ -1,8 +1,5 @@
-# # 태깅
-# okt 말고
-# kkma/ komoran로 바꾼 파일 
+# 부족했던 데이터 추가하여 진행!
 
-####### kkma.morphs 사용하기로 함!!!
 
 # ------------------------------------------------------------
 import numpy as np
@@ -10,27 +7,27 @@ import pandas as pd
 
 # up, down 불러와서 합치자
 # up
-up_data = pd.read_csv('../NLP/save/up_data_01.csv', index_col=0)
+up_data = pd.read_csv('../NLP/save/up_data_02.csv', index_col=0)
 print('load한 존대말 데이터: \n', up_data[-5:])
 # down
-down_data = pd.read_csv('../NLP/save/down_data_01.csv', index_col=0)
-print('load한 반말 데이터: \n', down_data[-5:])
+down_data = pd.read_csv('../NLP/save/down_data_02.csv', index_col=0)
+print('load한 반말 데이터: \n', down_data[-10:-5])
 # 각 데이터의 수 확인
 print('존댓말 데이터의 수: ', len(up_data), '\n반말 데이터의 수: ', len(down_data))
-# 존댓말 데이터의 수:  1750
-# 반말 데이터의 수:  2330
+# 존댓말 데이터의 수:  1750 > 2830
+# 반말 데이터의 수:  2330 > 2870
 
 # 두 데이터 합하기
 all_data = pd.concat([up_data, down_data])
 print('shape of all_data: ', all_data.shape)
-# shape of all_data:  (4080, 2)
+# shape of all_data:  (5700, 2)
 
 # ------------------------------------------------------------
 # 전처리 전에 pandas_profiling 으로 데이터 분석
 # import pandas_profiling
 
 # profile_all_data = all_data.profile_report()
-# profile_all_data.to_file('../NLP/sample_data/profile_all_data.html')
+# profile_all_data.to_file('../NLP/sample_data/profile_all_data_p_08.html')
 # print('===== save complete =====')
 
 
@@ -58,12 +55,27 @@ for sentence in all_data['data']:
     tag_data.append(temp_x)
 
 # 확인용 출력
-print('토큰화 된 샘플: ', tag_data[-5:])
+# print('토큰화 된 샘플: ', tag_data[-10:-5])
+
+### 불용어 제거 , 토큰화 전 ###
+#      label                        data
+# 35      0                몇 비비가 있다 그랬나
+# 36      0  내 친구는 뭐 컴활 이런 게 더 어렵다고 그랬나
+# 37      0                  실추라고 뭐 그랬나
+# 39      0                         그랬나
+# 42      0          선훈이 오빠가 여동생 있다 그랬나
+
+### 불용어 제거 , 토큰화 후 ###
+# 토큰화 된 샘플:  [['비', '비', '가', '있다', '그리하', '였', '나'], 
+                # ['내', '친구', '는', '뭐', '컴', '활', '이렇', 'ㄴ', '더', '어렵', '다고', '그리하', '였', '나'], 
+                # ['실추', '라고', '뭐', '그리하', '였', '나'], 
+                # ['그리하', '였', '나'], 
+                # ['선훈', '오빠', '가', '여동생', '있다', '그리하', '였', '나']]
 
 # ------------------------------------------------------------
 # 0번짜리 패딩과 OOV 토큰 고려하여 단어 집합의 크기에서 +2
-# 단어 집합(vocabulary)의 크기 : 4998
-vocab_size = 4998 + 2
+# 단어 집합(vocabulary)의 크기 : 5702
+vocab_size = len(tag_data) + 2
 
 # ------------------------------------------------------------
 # 정수 인코딩
@@ -77,42 +89,52 @@ tag_data = tokenizer.texts_to_sequences(tag_data)
 
 # 인코딩 확인을 위해 샘플 출력
 print(tag_data[:3])
-# [[1933, 509, 1133, 1934], [266, 1935], [267, 1134, 266, 3, 220, 365, 1936]]
+# [[1331, 7, 20, 389, 2054, 5, 505, 661, 1331, 7, 8, 49], [4, 4, 1332, 110, 506, 332, 202, 110, 49], [113, 2055, 5, 20, 49]]
 
 # ------------------------------------------------------------
 # x, y 로 지정해서 진행
 x = tag_data
 y = all_data['label']
 # 한 번 확인
-# print(len(x))     # 4080
-# print(len(y))     # 4080
+print(len(x))       # 5700
+print(len(y))       # 5700
 
 # ------------------------------------------------------------
 # 패딩(서로 다른 길이의 샘플을 동일하게 맞추기)
 
-# 길이 15으로 정해서 함수 돌리기
-max_len = 15
+
+#####
+# 패딩 왜 이 길이로 했는지 보여주려고~~~~
+
+
+
+
+
+
+# 길이 20으로 정해서 함수 돌리기
+max_len = 20
 
 # 패딩 적용
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 x = pad_sequences(x, maxlen = max_len, padding='pre') 
 # 확인
 print(x[0])
-# [   0    0    0    0    0    0    0    0    0    0    0 1933  509 1133  1934]
+# [  0  0  0  0  0  0  0  0 1331  7  20  389 2054  5  505  661 1331    7    8   49]
 
 # ------------------------------------------------------------
 # train, test 나누기
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, shuffle=True, random_state=1)
 # 확인
-# print(x_train.shape)
-# print(y_train.shape)
-# print(x_test.shape)
-# print(y_test.shape)
-# (3264, 15)
-# (3264,)
-# (816, 15)
-# (816,)
+print(x_train.shape)
+print(y_train.shape)
+print(x_test.shape)
+print(y_test.shape)
+# (4560, 20)
+# (4560,)
+# (1140, 20)
+# (1140,)
+
 
 # ------------------------------------------------------------
 # 모델 구성
@@ -122,22 +144,22 @@ from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 
 # 임베딩 벡터 차원 100으로 정하고 lstm 이용
 model = Sequential()
-model.add(Embedding(vocab_size, 50))
+model.add(Embedding(vocab_size, 100))
 model.add(LSTM(128))
 model.add(Dense(1, activation='sigmoid'))
 
 # callbacks 정의
 stop = EarlyStopping(monitor='val_loss', mode = 'min', verbose=1, patience=8)
-file_path = '../NLP/modelcheckpoint/project_07kkma-2.h5'
+file_path = '../NLP/modelcheckpoint/project_08-2.h5'
 mc = ModelCheckpoint(filepath= file_path, monitor='val_acc', mode = 'max', save_best_only=True, verbose=1)
 
 # 컴파일, 훈련
 model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
-# history = model.fit(x_train, y_train, epochs=15, batch_size=32, validation_split=0.2, callbacks=[stop, mc])
+history = model.fit(x_train, y_train, epochs=15, batch_size=32, validation_split=0.2, callbacks=[stop, mc])
 
 # ------------------------------------------------------------
 # 정확도가 가장 높은 가중치 가져와서 적용
-loaded_model = load_model('../NLP/modelcheckpoint/project_07kkma-2.h5')
+loaded_model = load_model('../NLP/modelcheckpoint/project_08-2.h5')
 print('===== save complete =====')
 print('loss: %.4f' % (loaded_model.evaluate(x_test, y_test)[0]), '\nacc: %.4f' % (loaded_model.evaluate(x_test, y_test)[1]))
 
@@ -145,7 +167,7 @@ print('loss: %.4f' % (loaded_model.evaluate(x_test, y_test)[0]), '\nacc: %.4f' %
 # 전에 predict에 넣을 것도 전처리 똑같이 해줘야 겠지?
 import re
 
-def sentiment_predict(new_sentence):
+def do_predict(new_sentence):
     # 전처리
     new_sentence = re.compile("[^ㄱ-ㅎㅏ-ㅣ가-힣]").sub(' ',new_sentence)
     new_sentence = kkma.morphs(new_sentence)  # 토큰화
@@ -164,56 +186,36 @@ def sentiment_predict(new_sentence):
 # =========================================================
 # 예측하기
 
-# project_07kkma.h5
-# loss: 0.0481 
-# acc: 0.9828
-
-# ['겨울', '지나가', '었', '다'] 는 99.22 % 확률로 반말입니다.
-# ['이렇', '게', '반말', '어도', '는', '모르', '겠', '지', 'ㅋㅋㅋ'] 는 91.41 % 확률로 반말입니다.
-# ['머신', '알', '아서', '잘하'] 는 96.06 % 확률로 반말입니다.
-# ['벌써', '금요일', '라니', '말', '도', '안', '되'] 는 62.46 % 확률로 반말입니다.
-# --------------------------------------------
-# ['겨울', '지나가', '었', '습니다'] 는 99.99 % 확률로 존댓말입니다.
-# ['겨울', '은', '지나가', '었', '다', 'ㅂ시다'] 는 92.47 % 확률로 존댓말입니다.
-# ['아버지', '가방', '들어가', '시', 'ㄴ다'] 는 70.35 % 확률로 존댓말입니다.
-# ['배', '고프', '지', '않', '나요'] 는 96.87 % 확률로 반말입니다.
-# ['이러', 'ㄹ', '배', '고프', '지요'] 는 78.70 % 확률로 존댓말입니다.
-# ['이러', 'ㄹ', '배', '고프', '죠'] 는 63.41 % 확률로 존댓말입니다.
-# ['는', '배', '고프', '아요'] 는 99.95 % 확률로 존댓말입니다.
-# ['어서', '점심', '시키', '세요'] 는 100.00 % 확률로 존댓말입니다.
-
-
-# project_07kkma.h5
-# loss: 0.0306
-# acc: 0.9914
-
-# ['겨울', '지나', '갔', '다'] 는 99.25 % 확률로 반말입니다.
-# ['내', '이렇', '게', '반말', '아도', '넌', '모르겠지ㅋㅋㅋ'] 는 94.57 % 확률로 반말입니다.
-# ['머신', '알', '아서', '잘'] 는 99.59 % 확률로 반말입니다.
-# ['벌써', '금요일', '라니', '말', '도', '안', '되'] 는 85.39 % 확률로 반말입니다.
-# --------------------------------------------
-# ['겨울', '지나', '갔', '습니다'] 는 99.97 % 확률로 존댓말입니다.
-# ['겨울', '은', '지나', '갔', '다', 'ㅂ시다'] 는 64.15 % 확률로 존댓말입니다.
-# ['아버지', '가방', '들어가', '시', 'ㄴ다'] 는 68.86 % 확률로 존댓말입니다.
-# ['배', '고프', '지', '않', '나요'] 는 97.21 % 확률로 반말입니다.
-# ['이렇', 'ㄹ', '배', '고프', '지요'] 는 78.84 % 확률로 반말입니다.
-# ['이렇', 'ㄹ', '배', '고프', '죠'] 는 58.16 % 확률로 반말입니다.
-# ['는', '배', '고프', '아요'] 는 99.94 % 확률로 존댓말입니다.
-# ['어서', '점심', '시키', '시', '어요'] 는 99.99 % 확률로 존댓말입니다.
-
+# project_08.h5
+# loss: 0.0274
+# acc: 0.9904
 
 # =========================================================
-sentiment_predict('겨울이 지나갔다.')
-sentiment_predict('내가 이렇게 반말해도 넌 모르겠지ㅋㅋㅋ')
-sentiment_predict('머신아 알아서 잘하자?')
-sentiment_predict('벌써 금요일이라니 말도 안 돼!')
-print('--------------------------------------------')
-sentiment_predict('겨울이 지나갔습니다.')
-sentiment_predict('겨울은 지나갔다 합시다..')
-sentiment_predict('아버지가방에들어가신다.')
-sentiment_predict('배 고프지 않나요?')
-sentiment_predict('이럴때 배가 고프지요.')
-sentiment_predict('이럴때 배가 고프죠.')
-sentiment_predict('저는 배가 고파요.')
-sentiment_predict('어서 점심을 시키세요.')
-
+do_predict('이렇게 말해')
+do_predict('이렇게 말하곤 해')
+do_predict('이렇게 말해볼까')
+do_predict('이렇게 말하니까')
+do_predict('이렇게 말하는거 어떠냐')
+do_predict('이렇게 말하는구나')
+do_predict('이렇게 말하지마')
+do_predict('이렇게 말하잖아')
+do_predict('이렇게 말하더라')
+do_predict('이렇게 말하잖니')
+do_predict('이렇게 말하네')
+do_predict('이렇게 말했어')
+do_predict('이렇게 말하자')
+do_predict('이렇게 말하면 안돼')
+print('-------------------------------')
+do_predict('이렇게 말해요')
+do_predict('이렇게 말했어요')
+do_predict('이렇게 말했다니까요')
+do_predict('이렇게 말했나요?')
+do_predict('이렇게 말하지요')
+do_predict('이렇게 말했잖아요')
+do_predict('이렇게 말해보세요')
+do_predict('이렇게 말한 거에요')
+do_predict('이렇게 말하지 마시오')
+do_predict('이렇게 말하죠')
+do_predict('이렇게 말했죠')
+do_predict('이렇게 말했습니다')
+do_predict('이렇게 말합시다')
